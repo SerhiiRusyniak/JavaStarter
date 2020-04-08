50cb4179bca440093addaa4e198e5888fcdbdbb6
SerhiiRusyniak <63352933+SerhiiRusyniak@users.noreply.github.com>
2020-04-08 19:37:33 +0300

    firs commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	int i = 10;
+	int a = 123;
+	int sum = i+a;
+        System.out.println(sum);
+    }
+}
